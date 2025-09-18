54d3f5472ffa985aa52118bca0859f2ebd2c0c33
adityasaini-primotech <aditya.saini@primotech.com>
2025-09-18 17:10:39 +0530

    added code for top app bar

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,16 +1,43 @@
 import { Ionicons } from "@expo/vector-icons";
 import { Tabs } from "expo-router";
+import { TouchableOpacity } from 'react-native';
 export default function RootLayout() {
-  
   return (
-    <Tabs >
-      <Tabs.Screen name="index" options={{ title: "Home",tabBarShowLabel: false, tabBarIcon: ({ color, focused }) => (
-          <Ionicons name={focused ? 'home-sharp' : 'home-outline'} color={color} size={24} />
-        ), }} />
-      <Tabs.Screen name="store" options={{ title: "Store",tabBarShowLabel: false, tabBarIcon: ({ color, focused }) => (
-          <Ionicons name={focused ? 'cart-sharp' : 'cart-outline'} color={color} size={24} />
-        ), }} />
+    <Tabs screenOptions={{ tabBarShowLabel: false }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons name={focused ? 'home-sharp' : 'home-outline'} color={color} size={24} />
+          ),
+          headerShown: true,
+          headerTitle: 'Devki-Your Milky Way',
+          headerStyle: { backgroundColor: '#6200ee' },
+          headerTintColor: '#fff',
+          headerRight: () => (
+            <TouchableOpacity onPress={() => {alert('Profile pressed');}}>
+              <Ionicons name="person-circle" size={36} color="#fff" />
+            </TouchableOpacity>
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="store"
+        options={{
+          title: "Store",
+          headerTitleStyle: { color: '#fff' },
+          headerStyle: { backgroundColor: '#6200ee' },
+          headerLeft: () => (
+            <TouchableOpacity onPress={() => {alert('Back pressed');}}>
+              <Ionicons name="arrow-back" size={24} color="#fff" style={{ marginLeft: 15 }} />
+            </TouchableOpacity>
+          ),
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons name={focused ? 'cart-sharp' : 'cart-outline'} color={color} size={24} />
+          ),
+            headerShown: true,
+          }}
+      />
     </Tabs>
   );
 }
-  
--- app/(tabs)/store.tsx
+++ app/(tabs)/store.tsx
@@ -1,9 +1,30 @@
 import { StyleSheet, Text, View } from 'react-native';
-
+import { SearchBar } from 'react-native-elements';
 export default function Store() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Store screen</Text>
+    
+      <SearchBar
+        placeholder="Search for products..."
+        lightTheme
+        round
+        platform='android'
+        containerStyle={{ backgroundColor: '#FFFFFF', borderTopWidth: 0, borderBottomWidth: 0, borderRadius: 10 , margin: 10 }}
+        inputContainerStyle={{ backgroundColor: '#FFFFFF', borderRadius: 30 , borderColor:"black", borderWidth: 1}}
+        inputStyle={{ color: '#000' }}
+
+      />
+
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center',backgroundColor: '#ffffff', width: '90%', marginTop: 20, borderRadius: 10, padding: 20 }}>
+        <View>
+          <Text >Welcome to the Store!</Text>
+        </View>
+        <View style={{ marginTop: 10 }}>
+          <Text >Browse and purchase your favorite products.</Text>
+        </View>
+        
+      </View>
+
     </View>
   );
 }
@@ -11,8 +32,7 @@ export default function Store() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#25292e',
-    justifyContent: 'center',
+    backgroundColor: '#fff',
     alignItems: 'center',
   },
   text: {
